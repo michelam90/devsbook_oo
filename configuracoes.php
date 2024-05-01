4ad30732f29aa791e0fc5ec46966c60f92a51688
Michel Machado <michel.a.m90@gmail.com>
2024-05-01 15:26:27 -0300

    Ajustes nos modulo de configurações (atualização de dados do usuário)

--- configuracoes.php
+++ configuracoes.php
@@ -18,44 +18,53 @@ require 'partials/menu.php';
 <section class="feed mt-10">
 
     <h1>Configurações</h1>
+    <?php
+        if(!empty($_SESSION['flash'])) {
+            echo $_SESSION['flash'];
+            $_SESSION['flash'] = '';
+        }
+    ?>
+
 
     <form method="POST" class="config-form" enctype="multipart/form-data" action="configuracoes_action.php">
 
         <label>
             Novo Avatar:<br/>
-            <input type="file" name="avatar" />
+            <input type="file" name="avatar" /> <br/>
+            <img class="mini" src="<?=$base;?>/media/avatars/<?=$userInfo->avatar;?>"
         </label>
 
         <label>
             Nova Capa:<br/>
-            <input type="file" name="cover" />
+            <input type="file" name="cover" /> <br/>
+            <img class="mini" src="<?=$base;?>/media/covers/<?=$userInfo->cover;?>"
         </label>
 
         <hr/>
 
         <label>
             Nome completo:<br/>
-            <input type="text" name="name" />
+            <input type="text" name="name" value="<?=$userInfo->name;?>" />
         </label>
 
         <label>
             E-mail:<br/>
-            <input type="email" name="email" />
+            <input type="email" name="email" value="<?=$userInfo->email;?>" />
         </label>
 
         <label>
             Data de Nascimento:<br/>
-            <input type="text" name="birthdate" />
+            <input type="text" name="birthdate" id="birthdate" value="<?=date('d/m/Y', strtotime($userInfo->birthdate));?>" />
         </label>
 
         <label>
             Cidade:<br/>
-            <input type="text" name="city" />
+            <input type="text" name="city" value="<?=$userInfo->city;?>" />
         </label>
 
         <label>
             Trabalho:<br/>
-            <input type="text" name="work" />
+            <input type="text" name="work" value="<?=$userInfo->work;?>" />
         </label>
 
         <hr/>
@@ -77,7 +86,13 @@ require 'partials/menu.php';
         
         
 </section>
-
+<script src="https://unpkg.com/imask@7.4.0/dist/imask.js"> </script>
+<script>
+    IMask (
+        document.getElementById("birthdate"),
+        {mask: '00/00/0000'}
+    );
+</script>
 
 <?php 
 require 'partials/footer.php';
